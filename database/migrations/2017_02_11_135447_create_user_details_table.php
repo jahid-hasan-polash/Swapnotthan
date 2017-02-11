d98f2b1d4d7d11d0c00166722a5709d7a97ff11a
Jahid Hasan Polash <jahidhasan@student.sust.edu>
2017-02-11 20:10:35 +0600

    updated 2

---
+++ database/migrations/2017_02_11_135447_create_user_details_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUserDetailsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_details', function (Blueprint $table) {
+            $table->increments('id');
+
+            $table->integer('user_id')->unsigned()->index();
+            $table->foreign('user_id')->references('id')
+                    ->on('users')->onDelete('cascade')->onUpdate('cascade');
+
+            $table->string('mother_name');
+            $table->string('father_name');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('user_details');
+    }
+}
